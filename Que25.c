480795d6e999e4253f2bfcf2e3d105c518aeb64c
chandni kumari jha <129723600+chandni210@users.noreply.github.com>
2023-10-18 09:43:16 +0530

    Add files via upload

---
+++ Que25.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int binarysearch(int arr[],int l,int r,int x)
+{
+    if(r>=1)
+    {
+       int mid =l+(r-l)/2;
+       if(arr[mid]==x)
+       
+        return mid;
+       
+       if(arr[mid]>x)
+       
+        return  binarysearch( arr,l,mid-1,x);
+        return  binarysearch( arr,mid+1,r,x);
+       
+    }
+    return -1;
+    
+}
+ int main()
+ {
+    int arr[]={2,3,4,10,40};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int x=10;
+    int result= binarysearch( arr,0,n-1,x);
+    (result==-1)
+    ? printf("not found")
+    :printf("at index%d",result);
+    return 0;
+ }
